d92f7321960df36cc5282d98966c99c671d274a8
Yasin9030 <yasinshaikh2186@gmail.com>
2025-08-11 18:43:13 +0530

    test2

--- backend/src/seed.ts
+++ backend/src/seed.ts
@@ -6,7 +6,7 @@ import { UserModel } from './models/User';
 async function run() {
   const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/QuickCourt';
   await connectDB(MONGO_URI);
-
+//34
   const email = 'aniket93yadav@gmail.com';
   const password = 'Aniket@0025';
 
